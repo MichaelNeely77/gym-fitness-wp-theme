87d5996dfce69887fb73d437ca49b460b691f4ee
Michael Neely <michael@michaelneely.net>
2020-07-25 00:05:18 -0400

    Added normalize.css

--- functions.php
+++ functions.php
@@ -12,3 +12,9 @@ function gymfitness_menus() {
 }
 
 add_action('init', 'gymfitness_menus');
+
+// Adds stylesheets and JS files
+function gymfitness_enqueue_scripts() {
+    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.01' );
+}
+add_action('wp_enqueue_scripts', 'gymfitness_enqueue_scripts');
