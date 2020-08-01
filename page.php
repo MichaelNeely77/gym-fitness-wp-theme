59d00e53c89fa407791080d903cad46330f2c7b9
Michael Neely <michael@michaelneely.net>
2020-08-01 00:00:51 -0400

    Added utilities for the page template

--- page.php
+++ page.php
@@ -1,21 +1,14 @@
 <?php get_header(); ?>
 
-<main class="container page section">
+<main class="container page section no-sidebars">
 
     <?php while(have_posts() ): the_post(); ?>
         <h1 class="text-center text-primary"><?php the_title(); ?></h1>
         <?php if(has_post_thumbnail() ) :
-            the_post_thumbnail('blog');
-        else:
-            echo "There is no image";
+            the_post_thumbnail('blog', array('class'=> 'featured-image'));
         endif;
-            
-         ?>
-            <div class="text-center">
-                <?php the_content(); ?>
-            </div>
-
-    <?php endwhile; ?>
+          the_content(); 
+        endwhile; ?>
 
 </main>
 
