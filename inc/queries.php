2503a5777211459d0af93db081c370a72c59bb5e
Michael Neely <michael@michaelneely.net>
2020-08-07 23:29:15 -0400

    Added the content to the queries.php page

--- inc/queries.php
+++ inc/queries.php
@@ -1,5 +1,28 @@
 <?php 
 
 function gymfitness_classes_list() {?>
-    
+    <ul class="classes-list">
+        <?php
+            $args = array(
+                'post_type' => 'gymfitness_classes',
+
+            );
+            $classes = new WP_Query($args);
+
+            while($classes->have_posts()): $classes->the_post();
+        ?>
+        <li class="gym-class card gradient">
+        <?php the_post_thumbnail( 'mediumSize' ); ?>
+       
+        <div class="card-content">
+            <a href="<?php the_permalink(); ?>">
+                <h3><?php the_title(); ?></h3>
+            </a>
+             
+        </div>
+        </li>
+
+            
+        <?php endwhile; ?>
+    </ul>
 <?php }
