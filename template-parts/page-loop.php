345832269156da1f62be8cb62975914cf3154ae8
Michael Neely <michael@michaelneely.net>
2020-08-11 23:51:40 -0400

    Added more style to the classes single page

--- template-parts/page-loop.php
+++ template-parts/page-loop.php
@@ -3,5 +3,13 @@
         <?php if(has_post_thumbnail() ) :
             the_post_thumbnail('blog', array('class'=> 'featured-image'));
         endif;
+          if(get_post_type() === 'gymfitness_classes'): 
+            
+                    $start_time = get_field('start_time');
+                    $end_time = get_field('end_time_');
+                ?>
+             <p class="content-class"><?php echo the_field('class_days') . " - " . $start_time . ' to ' . $end_time ?></p>
+          <?php endif;
+
           the_content(); 
         endwhile; ?>
