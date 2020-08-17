8bd8dd9e75fdf938b002b117fa607bd3786633f0
Michael Neely <michael@michaelneely.net>
2020-08-16 23:52:05 -0400

    Added the logic for the gallery

--- gallery.php
+++ gallery.php
@@ -7,7 +7,30 @@ get_header() ?>
     <main class="container page section">
         <?php while(have_posts() ): the_post(); ?>
             <h1 class="text-center text-primary"><?php the_title(); ?></h1>
-        <?php    the_content(); 
-        endwhile; ?>
+
+
+        <?php
+            $gallery = get_post_gallery( get_the_id(), false );
+
+            $gallery_images_ids = explode(',', $gallery['ids'] );
+
+            
+
+
+        ?>
+        <ul class="gallery-images">
+            <?php 
+                $i=0;
+                foreach($gallery_images_ids as $id): 
+                $size = ($i === 3 || $i === 6) ? 'portrait' : 'square';
+                    $image = wp_get_attachment_image_src($id, $size); 
+                    
+                    ?>
+                <img src="<?php echo $image[0] ?>" alt="">
+           
+
+                <?php $i++; endforeach; ?>
+        </ul>
+        <?php endwhile; ?>
     </main>
 <?php get_header(); ?>
