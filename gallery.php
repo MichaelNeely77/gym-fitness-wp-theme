4d1944488cb7d07576ffca593c029824de1a7d20
Michael Neely <michael@michaelneely.net>
2020-08-19 00:30:33 -0400

    Added the lightbox feature and found an error I didn't know I made.

--- gallery.php
+++ gallery.php
@@ -23,11 +23,12 @@ get_header() ?>
                 $i = 0;
                 foreach($gallery_images_ids as $id): 
                 $size = ($i === 3 || $i === 6) ? 'portrait' : 'square';
-                    $image = wp_get_attachment_image_src($id, $size); 
+                    $image = wp_get_attachment_image_src($id, 'large'); 
+                    $imageThumb = wp_get_attachment_image_src($id, $size); 
                     
                     ?>
                     <a href="<?php echo $image[0]; ?>" data-lightbox="gallery">
-                        <img src="<?php echo $image[0]; ?>" alt="">
+                        <img src="<?php echo $imageThumb[0]; ?>" alt="">
                     </a>
                     
 
@@ -38,4 +39,4 @@ get_header() ?>
         </ul>
         <?php endwhile; ?>
     </main>
-<?php get_header(); ?>
+<?php get_footer(); ?>
