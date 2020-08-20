b7cddbe02d41669a69b6a1c139a12ab5124e4b2c
Michael Neely <michael@michaelneely.net>
2020-08-19 23:46:44 -0400

    Added the rest of the blog page layout

--- home.php
+++ home.php
@@ -2,29 +2,29 @@
 
 <main class="container page section">
     <ul class="blog-entries">
-
-
-    </ul>
-    <?php while(have_posts() ): the_post(); ?>
-        <li class="card gradient">
-            <?php the_post_thumbnail('mediumSize'); ?>
-            <div class="card-content">
-                <a href="<?php the_permalink();  ?>">
-                    <h3><?php the_title(); ?></h3>
-                </a>
-                <p class="meta"><span>By: </span>
-                    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID') ); ?>">
-                        <?php echo get_the_author_meta('display_name'); ?>
+        <?php while(have_posts() ): the_post(); ?>
+            <li class="card gradient">
+                <?php the_post_thumbnail('mediumSize'); ?>
+                <div class="card-content">
+                    <a href="<?php the_permalink();  ?>">
+                        <h3 class="blog-page-title"><?php the_title(); ?></h3>
                     </a>
-                </p>
-                <p class="date-published">
-                    <?php the_time( get_option('date_format') ); ?>
-                </p>
-            </div>
+                    <p class="meta"><span>By: </span>
+                        <a href="<?php echo get_author_posts_url(get_the_author_meta('ID') ); ?>">
+                            <?php echo get_the_author_meta('display_name'); ?>
+                        </a>
+                    </p>
+                    <p class="date-published meta">
+                        <span>Date: </span>
+                        <?php the_time( get_option('date_format') ); ?>
+                    </p>
+                </div>
 
-        </li>
+            </li>
 
-    <?php endwhile; ?>
+        <?php endwhile; ?>
+    </ul>
+
 </main>
 
 
