3ce6054da709a248837c05cabed2a4d163f84b5c
Michael Neely <michael@michaelneely.net>
2020-08-19 23:27:27 -0400

    Added the logic for the blog posts on the blog page

--- home.php
+++ home.php
@@ -1,8 +1,28 @@
 <?php get_header(); ?>
 
 <main class="container page section">
+    <ul class="blog-entries">
+
+
+    </ul>
     <?php while(have_posts() ): the_post(); ?>
-        <h2><?php the_title(); ?></h2>
+        <li class="card gradient">
+            <?php the_post_thumbnail('mediumSize'); ?>
+            <div class="card-content">
+                <a href="<?php the_permalink();  ?>">
+                    <h3><?php the_title(); ?></h3>
+                </a>
+                <p class="meta"><span>By: </span>
+                    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID') ); ?>">
+                        <?php echo get_the_author_meta('display_name'); ?>
+                    </a>
+                </p>
+                <p class="date-published">
+                    <?php the_time( get_option('date_format') ); ?>
+                </p>
+            </div>
+
+        </li>
 
     <?php endwhile; ?>
 </main>
