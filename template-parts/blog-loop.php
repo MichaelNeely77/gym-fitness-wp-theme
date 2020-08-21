c5e3a6d53041752c9bfc22cd2561f8c1d324c657
Michael Neely <michael@michaelneely.net>
2020-08-20 23:43:25 -0400

    Addedd the category tags in the cards.

--- template-parts/blog-loop.php
+++ template-parts/blog-loop.php
@@ -2,6 +2,8 @@
         <?php while(have_posts() ): the_post(); ?>
             <li class="card gradient">
                 <?php the_post_thumbnail('mediumSize'); ?>
+
+                <?php the_category();?>
                 <div class="card-content">
                     <a href="<?php the_permalink();  ?>">
                         <h3 class="blog-page-title"><?php the_title(); ?></h3>
